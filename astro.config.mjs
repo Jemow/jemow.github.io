db3db31a71b51d197013264e318e3a5fcd372aba
Jemo <103411555+Jemow@users.noreply.github.com>
2026-02-04 22:37:20 +0100

    Fix Astro base

--- astro.config.mjs
+++ astro.config.mjs
@@ -12,7 +12,7 @@ import icon from "astro-icon";
 // https://astro.build/config
 export default defineConfig({
   site: "https://jemo.github.io",
-  base: '/jemo.github.io/',
+  base: '/',
   integrations: [mdx(), sitemap(), pagefind(), icon()],
 
   vite: {
